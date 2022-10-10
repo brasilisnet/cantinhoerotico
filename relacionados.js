05648ddfbcd9e3539966976dc08b53bb6b5dd65b
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2022-10-10 21:37:34 +0900

    Add files via upload

---
+++ relacionados.js
@@ -0,0 +1,5 @@
+var currentposturl=&quot;<data:post.url/>&quot;;
+var maxresults=5;
+var relatedpoststitle=&quot;<b>Relacionados:</b>&quot;;
+removeRelatedDuplicates_thumbs();
+printRelatedLabels_thumbs();
