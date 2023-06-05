51acc852e48f0970a7c57e976dd6f0c1aa93a266
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2023-06-05 14:41:44 +0900

    Add files via upload

---
+++ babes18.js
@@ -0,0 +1,10 @@
+//Javascript Created by Computerhope https://www.computerhope.com/
+//store the quotations in arrays
+var images = [],
+index = 0;
+images[0] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/j7OUAfd.jpg' width='100%' height='auto'></a>";
+images[1] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/eOVfo0b.jpg' width='100%' height='auto'></a>";
+images[2] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/wjeJtCl.jpg' width='100%' height='auto'></a>";
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
+//done
