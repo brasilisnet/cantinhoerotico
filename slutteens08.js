df10c68174d0581dc38fdc91ed52862dcebec6ef
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2023-07-09 21:41:10 +0900

    Add files via upload

---
+++ slutteens08.js
@@ -0,0 +1,10 @@
+//Javascript Created by Computerhope https://www.computerhope.com/
+//store the quotations in arrays
+var images = [],
+index = 0;
+images[0] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/dX8BghM.jpg' width='100%' height='auto'></a>";
+images[1] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/7wKBCyV.jpg' width='100%' height='auto'></a>";
+images[2] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/jYBZtvg.jpg' width='100%' height='auto'></a>";
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
+//done
---
+++ slutteens16.js
@@ -0,0 +1,10 @@
+//Javascript Created by Computerhope https://www.computerhope.com/
+//store the quotations in arrays
+var images = [],
+index = 0;
+images[0] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/GHbaIzr.jpg' width='100%' height='auto'></a>";
+images[1] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/E48vYtR.jpg' width='100%' height='auto'></a>";
+images[2] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/Abld1tQ.jpg' width='100%' height='auto'></a>";
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
+//done
