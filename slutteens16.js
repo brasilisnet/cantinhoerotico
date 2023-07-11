60d8346550be64acc23778077a2c9934b8bb93fd
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2023-07-11 10:59:23 +0900

    Update slutteens16.js

--- slutteens16.js
+++ slutteens16.js
@@ -3,7 +3,7 @@
 var images = [],
 index = 0;
 images[0] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/GHbaIzr.jpg' width='100%' height='auto'></a>";
-images[1] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/E48vYtR.jpg' width='100%' height='auto'></a>";
+images[1] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/MVbF0ju.jpg' width='100%' height='auto'></a>";
 images[2] = "<a target='_blank' href='https://shope.ee/3VDNHWRl8C'><img border='0' src='https://i.imgur.com/Abld1tQ.jpg' width='100%' height='auto'></a>";
 index = Math.floor(Math.random() * images.length);
 document.write(images[index]);
